ec2b7b8f57b7ce4f7b2676b2abefae7dd59274a1
Koks <naanoksa@gmail.com>
2022-05-07 12:21:23 +0300

    first commit

---
+++ Server/Server/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Server
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ChatServer cs;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lbActiveClients.SelectionChanged += (_s, _e) =>
+            {
+                if (lbActiveClients.SelectedValue == null)
+                    return;
+                if (lbActiveClients.SelectedValue is Client)
+                    tbTargetUsername.Text = (lbActiveClients.SelectedValue as Client).Username;
+            };
+
+
+            this.DataContext = cs = new ChatServer();
+        }
+
+        private void bSwitchServerState_Click(object sender, RoutedEventArgs e)
+        {
+            cs.SwitchServerState();
+        }
+
+        private void bSend_Click(object sender, RoutedEventArgs e)
+        {
+            cs.SendMessage(tbTargetUsername.Text, tbMessage.Text);
+        }
+    }
+}
